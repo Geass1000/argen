27d27dce7ce980f205c4349a66f5b8817a09d554
Geass <geass1000@gmail.com>
2018-05-11 00:49:34 +0300

    add: `get` method (stub)

--- src/@core/di/container.di.ts
+++ src/@core/di/container.di.ts
@@ -8,4 +8,12 @@ export class Container {
 
     public addInjector (injector: Interfaces.DI.Injector) {
     }
+
+    /**
+     * Returns the instance of the DI Entity.
+     *
+     * @return {type}
+     */
+    public get (): any {
+    }
 }
