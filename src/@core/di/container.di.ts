483f41683714615c51be3314041b2471e1d0a9ae
Geass <geass1000@gmail.com>
2018-05-11 00:13:28 +0300

    add: base DI Container

---
+++ src/@core/di/container.di.ts
@@ -0,0 +1,11 @@
+import * as Interfaces from '../interfaces';
+
+export class Container {
+    private store: any;
+
+    public addEntity (entity: Interfaces.DI.Entity) {
+    }
+
+    public addInjector (injector: Interfaces.DI.Injector) {
+    }
+}
